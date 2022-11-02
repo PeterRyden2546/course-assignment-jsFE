fe703dcc65fe61944c6b15a3637b5cf1b9652420
Peter Rydden <peter.ryden@utb.ecutbildning.se>
2022-11-02 11:44:05 +0100

    retrieve element form api and stored in variables. and headlind in html

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,20 @@ const urlApi = 'https://api.nytimes.com/svc/topstories/v2/world.json?api-key=lu8
 
 let data = fetch(urlApi)
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    data.results.forEach(element => {
+   if(element.multimedia == null){
+   element.multimedia = [{url: 'https://upload.wikimedia.org/wikipedia/commons/2/26/512pxIcon-sunset_photo_not_found.png'}]; 
+ }
+ console.log(element);
+ let title = element.title;
+ let img = element.multimedia[0].url;
+ let section = element.section;
+ let subsection = element.subsection;
+ let publishedTime = element.published_date;
+ let updatedTime = element.updated_date;
+ let articleUrl = element.url;
+ let moreInfo = element.abstract;
+ let reporter = element.byline;
+ });
+});
